d686cd959b32d9559fff3b2e3b89d3c12e4ed330
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2023-10-24 18:34:57 +0530

    Updated channels: Best

--- src/model.py
+++ src/model.py
@@ -9,13 +9,13 @@ class MyModel(nn.Module):
 
         # Define a CNN architecture with batch normalization and kernel size (1, 1) for "max-pooling".
         self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
-        self.conv3 = nn.Conv2d(128, 64, 3, padding=1)
+        self.conv2 = nn.Conv2d(64, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
         self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
 
-        self.fc1 = nn.Linear(128 * 14 * 14, 1028)
-        self.fc2 = nn.Linear(1028, 256)
-        self.fc3 = nn.Linear(256, num_classes)
+        self.fc1 = nn.Linear(128 * 14 * 14, 2048)
+        self.fc2 = nn.Linear(2048, 512)
+        self.fc3 = nn.Linear(512, num_classes)
         self.pool = nn.MaxPool2d(2, 2)
 
         self.dropout = nn.Dropout(p=dropout)

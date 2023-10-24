2ae7a751bd1cf6c7b48f5f5c165ce8e34257bfab
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2023-10-24 17:52:49 +0530

    Updated channels: Best

--- src/model.py
+++ src/model.py
@@ -9,8 +9,8 @@ class MyModel(nn.Module):
 
         # Define a CNN architecture with batch normalization and kernel size (1, 1) for "max-pooling".
         self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
-        self.conv2 = nn.Conv2d(64, 32, 3, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv3 = nn.Conv2d(128, 64, 3, padding=1)
         self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
 
         self.fc1 = nn.Linear(128 * 14 * 14, 1028)

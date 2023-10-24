14b23639dc46ed9c0ab221ea3d191a941954138d
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2023-10-24 10:05:27 +0530

    Modified architecture :calm:

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class MyModel(nn.Module):
@@ -7,58 +8,27 @@ class MyModel(nn.Module):
         super().__init__()
 
         # Define a CNN architecture with batch normalization and kernel size (1, 1) for "max-pooling".
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(kernel_size=2),  # No change in size
-            nn.ReLU(),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(kernel_size=2),  # No change in size
-            nn.ReLU(),
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.MaxPool2d(kernel_size=2),  # No change in size
-            nn.ReLU(),
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
 
-        )
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(256, 128, 3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.MaxPool2d(kernel_size=2),  # No change in size
-            nn.ReLU(),
-
-        )
-        self.conv5 = nn.Sequential(
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.MaxPool2d(kernel_size=2),  # No change in size
-            nn.ReLU(),
-        )
-
-        self.fc1 = nn.Linear(256 * 7 * 7, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, num_classes)
+        self.fc1 = nn.Linear(128 * 14 * 14, 1028)
+        self.fc2 = nn.Linear(1028, 256)
+        self.fc3 = nn.Linear(256, num_classes)
+        self.pool = nn.MaxPool2d(2, 2)
 
         self.dropout = nn.Dropout(p=dropout)
-        self.logSoftmax = nn.LogSoftmax(dim=1)
-
         self.flatten = nn.Flatten()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.conv1(x)
+        x = self.pool(F.relu(self.conv1(x)))
         print(x.shape)
-        x = self.conv2(x)
+        x = self.pool(F.relu(self.conv2(x)))
         print(x.shape)
-        x = self.conv3(x)
+        x = self.pool(F.relu(self.conv3(x)))
         print(x.shape)
-        x = self.conv4(x)
-        print(x.shape)
-        x = self.conv5(x)
+        x = self.pool(F.relu(self.conv4(x)))
         print(x.shape)
 
         x = self.flatten(x)

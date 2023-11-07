f0c5c9aec6711c99185d43783a78766f30cdf3dc
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2023-11-07 11:58:33 +0530

    Updated Gamma for ExponentialLR scheduler

--- src/train.py
+++ src/train.py
@@ -108,7 +108,7 @@ def optimize(data_loaders, model, optimizer, loss, n_epochs, save_path, interact
     # plateau
     # HINT: look here: 
     # https://pytorch.org/docs/stable/optim.html#how-to-adjust-learning-rate
-    scheduler = ExponentialLR(optimizer, gamma=0.3)
+    scheduler = ExponentialLR(optimizer, gamma=0.2)
 
     for epoch in range(1, n_epochs + 1):
 
